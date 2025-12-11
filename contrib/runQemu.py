faab6c4d05586b7871f67a869e4bba9fc9acb75d
Tom Braune <tom.braune@secunet.com>
2025-12-11 16:07:38 +0100

    Fix GptDiskPath for contrib

--- contrib/runQemu.py
+++ contrib/runQemu.py
@@ -95,13 +95,14 @@ def run_x86(image: Path):
             },
         ],
     }
+    os.makedirs(Path("./run").absolute(), exist_ok=True)
     vm = VmQemu(host_ref=h, id="run")
     vm.start(
-        workdir=".",
+        workdir=str(Path("./run").absolute()),
         image=image,
         serial_path="serial.out",
         netdev_extra_options=netdev_extra_opts,
-        disk_config=disk_config
+        disk_config=disk_config,
     )
     log.info(f"Artifacts directory is {vm.artifacts_path}")
     log.info("SSH root password is 'muen'")
@@ -143,7 +144,7 @@ log.info(f"Using dist dir '{dist_dir}'")
 stop_disable()
 if "x86" in args.query:
     log.info("Assuming x86 architecture")
-    run_x86(image=Path(dist_dir) / "muen.iso")
+    run_x86(image=(Path(dist_dir) / "muen.iso").absolute())
 elif "arm64" in args.query:
     log.info("Assuming arm64 architecture")
 
